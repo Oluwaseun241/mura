386990949a4a3f32659e8b21f097e341697781b0
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2024-10-06 08:54:29 +0100

    wip: label detection

--- go.mod
+++ go.mod
@@ -2,10 +2,7 @@ module github.com/Oluwaseun241/mura
 
 go 1.22.5
 
-require (
-	cloud.google.com/go/vision v1.2.0
-	google.golang.org/api v0.70.0
-)
+require cloud.google.com/go/vision v1.2.0
 
 require (
 	cloud.google.com/go v0.100.2 // indirect
@@ -14,11 +11,13 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.7 // indirect
 	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/api v0.70.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20220222213610-43724f9ea8cf // indirect
 	google.golang.org/grpc v1.44.0 // indirect
--- main.go
+++ main.go
@@ -1,54 +1,58 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
-	"os/exec"
+	"os"
+
+	vision "cloud.google.com/go/vision/apiv1"
+	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
 )
 
 func main() {
-	// loacl model(ollama)
-	imagePt := "./raw.jpeg"
-	cmd := exec.Command("ollama", "run", "llava", "--image", imagePt, "What food is in this image?")
+	ctx := context.Background()
 
-	// Capture the output
-	output, err := cmd.Output()
+	// Load Env variables
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Error running Ollama LLaVA: %v", err)
+		log.Fatal("Error loading .env file")
+	}
+	credsPath := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	if credsPath == "" {
+		log.Fatal("GOOGLE_APPLICATION_CREDENTIALS environment variable is not set.")
 	}
 
-	// Print the result
-	fmt.Println("LLaVA Response:", string(output))
+	// Initialize a Vision client with your credentials
+	client, err := vision.NewImageAnnotatorClient(ctx, option.WithCredentialsFile(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")))
+	if err != nil {
+		log.Fatalf("Failed to create Vision client: %v", err)
+	}
+	defer client.Close()
 
-	// ctx := context.Background()
-	//
-	// // Initialize a Vision client with your credentials
-	// client, err := vision.NewImageAnnotatorClient(ctx, option.WithCredentialsFile(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")))
-	// if err != nil {
-	// 	log.Fatalf("Failed to create Vision client: %v", err)
-	// }
-	// defer client.Close()
-	//
-	// // Read the image file
-	// imagePath, err := os.Open("./raw.jpeg")
-	// if err != nil {
-	// 	log.Fatalf("Failed to read image path: %v", err)
-	// }
-	// defer imagePath.Close()
-	//
-	// // Convert the image file to Google Vision's Image type
-	// image, _ := vision.NewImageFromReader(imagePath)
-	//
-	// // Perform label detection on the image
-	// labels, err := client.DetectLabels(ctx, image, nil, 10)
-	// if err != nil {
-	// 	log.Fatalf("Failed to detect labels: %v", err)
-	// }
-	//
-	// // Print the detected labels
-	// fmt.Println("Detected labels:")
-	// for _, label := range labels {
-	// 	fmt.Printf("%s (Confidence: %f)\n", label.Description, label.Score)
-	// }
+	// Read the image file
+	imagePath, err := os.Open("./raw.jpeg")
+	if err != nil {
+		log.Fatalf("Failed to read image path: %v", err)
+	}
+	defer imagePath.Close()
 
+	// Convert the image file to Google Vision's Image type
+	image, _ := vision.NewImageFromReader(imagePath)
+
+	// Perform label detection on the image
+	labels, err := client.LocalizeObjects(ctx, image, nil)
+	if err != nil {
+		log.Fatalf("Failed to detect labels: %v", err)
+	}
+
+	// Print the detected labels
+	fmt.Println("Detected labels:")
+	for _, label := range labels {
+		if label.Score <= 0.59 {
+			fmt.Printf("Not sure of %s", label.Name)
+		}
+		fmt.Printf("%s (Confidence: %f)\n", label.Name, label.Score)
+	}
 }
