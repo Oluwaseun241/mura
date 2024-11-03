fc2ad508eaae5bcb34b925c3f8cc02bf59a487b3
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2024-11-03 22:05:38 +0100

    chore: improving the prompt

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -13,8 +13,8 @@ import (
 func getFoodRecipes(ingredients []string, dish string) (string, error) {
 	ctx := context.Background()
 
-	prompt1 := fmt.Sprintf("You are a helpful, AI assistant devoted to providing accurate and delightful recipes.These are the guildlines for you to follow when delivering a recipe response to a request 1. List out the ingredients first,including quantities.Provide detailed cooking times, temperatures and any special kitchen equipment needed 2.Provide step-by-step instructions for prepping, mixing, cooking, plating and any other necessary steps, detailed enough to follow. Include safety tips and special techniques as applicable Here are the ingredients I have: %s. Can you give me a specific recipe that includes only these ingredients, and detailed preparation steps? Lastly Nutritional information like Calories, Protein and Carbs", strings.Join(ingredients, ", "))
-	prompt2 := fmt.Sprintf("You are a helpful, AI assistant devoted to providing accurate and delightful recipes.These are the guildlines for you to follow when delivering a recipe response to a request 1. List out the ingredients first,including quantities.Provide detailed cooking times, temperatures and any special kitchen equipment needed 2.Provide step-by-step instructions for prepping, mixing, cooking, plating and any other necessary steps, detailed enough to follow. Include safety tips and special techniques as applicable Here are the ingredients I have: %s. Can you give me a specific recipe that includes only these ingredients, Nutritional information like Calories, Protein and Carbs, and detailed preparation steps for %s", strings.Join(ingredients, ", "), dish)
+	prompt1 := fmt.Sprintf("You are a helpful, AI assistant devoted to providing accurate and delightful recipes.These are the guildlines for you to follow when delivering a recipe response to a request 1. List out the ingredients first,including quantities.Provide detailed cooking times, temperatures and any special kitchen equipment needed 2.Provide step-by-step instructions for prepping, mixing, cooking, plating and any other necessary steps, detailed enough to follow. Include Pro chef tips and special techniques as applicable Here are the ingredients I have: %s. Can you give me a specific recipe that includes only these ingredients, and detailed preparation steps? Lastly Nutritional information like Calories, Protein and Carbs", strings.Join(ingredients, ", "))
+	prompt2 := fmt.Sprintf("You are a helpful, AI assistant devoted to providing accurate and delightful recipes.These are the guildlines for you to follow when delivering a recipe response to a request 1. List out the ingredients first,including quantities.Provide detailed cooking times, temperatures and any special kitchen equipment needed 2.Provide step-by-step instructions for prepping, mixing, cooking, plating and any other necessary steps, detailed enough to follow. Include Pro chef tips and special techniques as applicable Here are the ingredients I have: %s. Can you give me a specific recipe that includes only these ingredients, Nutritional information like Calories, Protein and Carbs, and detailed preparation steps for %s", strings.Join(ingredients, ", "), dish)
 
 	// S.elect the appropriate prompt
 	var prompt string
@@ -37,12 +37,15 @@ func getFoodRecipes(ingredients []string, dish string) (string, error) {
 
 func detectFood(fileBytes []byte) (string, error) {
 	ctx := context.Background()
+
 	prompt := []genai.Part{
 		genai.ImageData("jpeg", fileBytes),
-		genai.Text("Accurately identify the food in the image and provide an appropriate recipe consistent with your analysis.These are the guildlines for you to follow when delivering a recipe response to a request 1. List out the ingredients first,including quantities.Provide detailed cooking times, temperatures and any special kitchen equipment needed 2.Provide step-by-step instructions for prepping, mixing, cooking, plating and any other necessary steps, detailed enough to follow. Include safety tips and special techniques as applicable. Lastly Nutritional information like Calories, Protein and Carbs"),
+		genai.Text("Accurately identify the food in the image and provide an appropriate recipe consistent with your analysis.These are the guildlines for you to follow when delivering a recipe response to a request 1. List out the ingredients first, including quantities. Provide detailed cooking times, temperatures and any special kitchen equipment needed 2.Provide step-by-step instructions for prepping, mixing, cooking, plating and any other necessary steps, detailed enough to follow. Include pro chef tips and special techniques as applicable. Lastly Nutritional information like Calories, Protein and Carbs"),
 	}
 
 	model := client.GeminiClient.GenerativeModel("gemini-1.5-pro")
+	//model.ResponseMIMEType = "application/json"
+
 	resp, err := model.GenerateContent(ctx, prompt...)
 	if err != nil {
 		return "", fmt.Errorf("Error generating content")
--- todo.md
+++ todo.md
@@ -5,9 +5,9 @@ TODO
 - [x] How to return non food uploads
 - [x] Youtube suggestions(search)
 - [x] Write test
-- [] Reduce response time (currently 20 - 45 sec for reicipe) (4-10 sec for ingredient)
-  from 20
-  from 4.92 sec to 1.4 sec for ingredient
+- [x] Reduce response time (currently 20 - 45 sec for reicipe) (4-10 sec for ingredient)
+      from 20
+      from 4.92 sec to 1.4 - 1.2 sec for ingredient
 - [] Image uri(thumbnail thing) with cloudinary(to train model too) -- future
 
 _Note: recipe steps is in markdown format_
