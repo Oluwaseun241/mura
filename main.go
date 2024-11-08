11939d7cb84d169f891216c0342202ac2c97f2b9
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2024-11-08 18:47:28 +0100

    fear: v.8.0

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	// Load Env variables
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	// initialize client connection
