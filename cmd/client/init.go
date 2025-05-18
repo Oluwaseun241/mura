45bbafb1fd6637167d1b4032748e454873e0e334
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2025-05-18 22:51:11 +0100

    new leaf

--- cmd/client/init.go
+++ cmd/client/init.go
@@ -5,13 +5,11 @@ import (
 	"log"
 	"os"
 
-	vision "cloud.google.com/go/vision/apiv1"
 	"github.com/google/generative-ai-go/genai"
 	"google.golang.org/api/option"
 )
 
 var (
-	VisionClient *vision.ImageAnnotatorClient
 	GeminiClient *genai.Client
 )
 
@@ -19,19 +17,12 @@ func Init() {
 	ctx := context.Background()
 	var err error
 
-	authKey := os.Getenv("GOOGLE_SERVICE_KEY")
 	geminiApiKey := os.Getenv("GEMINI_API_KEY")
 
-	if authKey != "" && geminiApiKey != "" {
-		VisionClient, err = vision.NewImageAnnotatorClient(ctx, option.WithAPIKey(authKey))
-		if err != nil {
-			log.Printf("Failed to create vision client: %v", err)
-		}
-
+	if geminiApiKey != "" {
 		GeminiClient, err = genai.NewClient(ctx, option.WithAPIKey(geminiApiKey))
 		if err != nil {
 			log.Printf("Failed to create gemini client: %v", err)
 		}
 	}
-
 }
